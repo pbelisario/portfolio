74b72d92888d2eab03d669ef9997fd74d019940f
pbelisario_win <pedrobelisario1997@gmail.com>
2020-05-12 00:42:10 -0300

    Updates

---
+++ precache-manifest.edc578ed721d6c6e409d38c1c5e40d14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87fc8d602b495d2cd3a062499c3b73b1",
+    "url": "/pbelisario.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "5185835ba6c1ecf6e298",
+    "url": "/pbelisario.github.io/portfolio/static/css/2.68bf7c02.chunk.css"
+  },
+  {
+    "revision": "cdbf36c9692e6d1ed3fd",
+    "url": "/pbelisario.github.io/portfolio/static/css/main.b1b136ab.chunk.css"
+  },
+  {
+    "revision": "5185835ba6c1ecf6e298",
+    "url": "/pbelisario.github.io/portfolio/static/js/2.d7cd0085.chunk.js"
+  },
+  {
+    "revision": "279cb6ac5078a77467e10c3ede790324",
+    "url": "/pbelisario.github.io/portfolio/static/js/2.d7cd0085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbf36c9692e6d1ed3fd",
+    "url": "/pbelisario.github.io/portfolio/static/js/main.6b5d40c7.chunk.js"
+  },
+  {
+    "revision": "7005012f920860a5e2bb",
+    "url": "/pbelisario.github.io/portfolio/static/js/runtime-main.a2f8dad8.js"
+  }
+]);
